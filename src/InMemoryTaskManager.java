a4d93c0219338918365dc221cebd510422f167ae
timur Bockarev <timurbockarev069@gmail.com>
2024-11-17 09:26:39 +0300

    tests's push

--- src/InMemoryTaskManager.java
+++ src/InMemoryTaskManager.java
@@ -2,7 +2,7 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Manager {
+public class InMemoryTaskManager implements TaskManager {
 
     private HashMap<Integer, Task> tasks = new HashMap<>();
     private HashMap<Integer, Epic> epics = new HashMap<>();
@@ -10,45 +10,54 @@ public class Manager {
 
     private int id = 1;
 
+    @Override
     public ArrayList<Task> getAllTasks() {
         return new ArrayList<>(tasks.values());
     }
 
     // Получение списка всех эпиков (Epic)
+    @Override
     public ArrayList<Epic> getAllEpics() {
         return new ArrayList<>(epics.values());
     }
 
     // Получение списка всех подзадач (Subtask)
+    @Override
     public ArrayList<Subtask> getAllSubtasks() {
         return new ArrayList<>(subtasks.values());
     }
+    @Override
     public void createTask(Task task) {
         task.setId(id++);
         task.setStatus(TaskStatus.NEW);
         tasks.put(task.getId(), task);
     }
 
+    @Override
     public void deleteTask(int id) {
         tasks.remove(id);
     }
 
+    @Override
     public void updateTask(Task task) {
         if (tasks.containsKey(task.getId())) {
             tasks.put(task.getId(), task);
         }
     }
 
+    @Override
     public void getTask(Task task) {
         tasks.get(task.getId());
     }
 
+    @Override
     public void removeTask(int id) {
         tasks.remove(id);
     }
 
 
     //----------------------------------------------------------------//
+    @Override
     public ArrayList<Subtask> getSubtasksByEpicId(int epicId) {
         ArrayList<Subtask> subtasksByEpic = new ArrayList<>();
 
@@ -62,6 +71,7 @@ public class Manager {
     }
 
 
+    @Override
     public void createSubtask(Subtask subtask) {
         subtask.setId(id++);
         subtask.setStatus(TaskStatus.NEW);
@@ -69,25 +79,30 @@ public class Manager {
         updateEpicStatus(subtask.getEpicId());
     }
 
+    @Override
     public void deleteSubtask(int id) {
         subtasks.remove(id);
     }
 
+    @Override
     public void updateSubtask(Subtask subtask) {
         if (subtasks.containsKey(subtask.getId())) {
             subtasks.put(subtask.getId(), subtask);
         }
     }
 
+    @Override
     public void getSubtask(Subtask subtask) {
         subtasks.get(subtask.getId());
     }
 
+    @Override
     public void removeSubtask(int id) {
         subtasks.remove(id);
     }
 
-    private void updateEpicStatus(int epicId) {
+    @Override
+    public void updateEpicStatus(int epicId) {
         Epic epic = epics.get(epicId);
         if (epic == null) return; // Эпик не найден
 
@@ -124,31 +139,33 @@ public class Manager {
     }
     //------------------------------------------------------------------//
 
-
-
-
+    @Override
     public void createEpic(Epic epic) {
         epic.setId(id++);
         epic.setStatus(TaskStatus.NEW);
         epics.put(epic.getId(), epic);
     }
 
+    @Override
     public void deleteEpic(int id) {
         epics.remove(id);
     }
 
     // Метод для вывода списка всех задач
 
+    @Override
     public void updateEpic(Epic epic) {
         if (epics.containsKey(epic.getId())) {
             epics.put(epic.getId(), epic);
         }
     }
 
+    @Override
     public Epic getEpicById(int id) {
         return epics.get(id);
     }
 
+    @Override
     public void removeEpic(int id) {
         epics.remove(id);
     }
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
-public class Main {
+  public class Main {
 
     public static void main(String[] args) {
-        Manager manager = new Manager();
+        InMemoryTaskManager inMemoryTaskManager = new InMemoryTaskManager();
 
         Task task1 = new Task("First task", TaskStatus.NEW, "Task_1");
         Task task2 =  new Task("it`s second pf tasks, i believ thats it great", TaskStatus.NEW, "Task_2" );
-        manager.createTask(task1);
-        manager.createTask(task2);
+        inMemoryTaskManager.createTask(task1);
+        inMemoryTaskManager.createTask(task2);
 
         Epic epic1 = new Epic("Epic 1", "Description of Epic 1", TaskStatus.NEW);
-        manager.createEpic(epic1);
+        inMemoryTaskManager.createEpic(epic1);
 
         Epic epic2 = new Epic("Epic 2", "Description of Epic 2", TaskStatus.NEW);
-        manager.createEpic(epic2);
+        inMemoryTaskManager.createEpic(epic2);
 
         Subtask subtask1_1 = new Subtask("Subtask 1.1", TaskStatus.NEW, "Subtask Title 1.1", "Title", "Description", TaskStatus.NEW);
         subtask1_1.setEpicId(epic1.getId()); // Присваиваем эпик к подзадаче
@@ -20,13 +20,13 @@ public class Main {
         Subtask subtask1_2 = new Subtask("Subtask 1.2", TaskStatus.NEW, "Subtask Title 1.2", "Title", "Description", TaskStatus.NEW);
         subtask1_2.setEpicId(epic1.getId()); // Присваиваем эпик к подзадаче
 
-        manager.createSubtask(subtask1_2);
-        manager.createSubtask(subtask1_1);
+        inMemoryTaskManager.createSubtask(subtask1_2);
+        inMemoryTaskManager.createSubtask(subtask1_1);
 
         // Создание подзадачи для второго эпика
         Subtask subtask2_1 = new Subtask("Subtask 2.1", TaskStatus.NEW, "Subtask Title 2.1", "Title", "Description", TaskStatus.NEW);
         subtask2_1.setEpicId(epic2.getId()); // Присваиваем эпик к подзадаче
-        manager.createSubtask(subtask2_1);
+        inMemoryTaskManager.createSubtask(subtask2_1);
 
         System.out.println("Tasks:");
         System.out.println(task1.getTitle() + ": " + task1.getDescription() + " (Status: " + task1.getStatus() + ")");
@@ -44,19 +44,19 @@ public class Main {
         System.out.println(subtask2_1.getTitle() + ": " + subtask2_1.getDescription() + " (Status: " + subtask2_1.getStatus() + ")");
         System.out.println("/-----------------------------/");
         System.out.println("Tasks:");
-        for (Task task : manager.getAllTasks()) {
+        for (Task task : inMemoryTaskManager.getAllTasks()) {
             System.out.println(task.getTitle() + ": " + task.getDescription() + " (Status: " + task.getStatus() + ")");
         }
 
         // Вывод всех эпиков
         System.out.println("Epics:");
-        for (Epic epic : manager.getAllEpics()) {
+        for (Epic epic : inMemoryTaskManager.getAllEpics()) {
             System.out.println(epic.getTitle() + ": " + epic.getDescription() + " (Status: " + epic.getStatus() + ")");
         }
 
         // Вывод всех подзадач
         System.out.println("Subtasks:");
-        for (Subtask subtask : manager.getAllSubtasks()) {
+        for (Subtask subtask : inMemoryTaskManager.getAllSubtasks()) {
             System.out.println(subtask.getTitle() + ": " + subtask.getDescription() + " (Status: " + subtask.getStatus() + ")");
         }
     }
